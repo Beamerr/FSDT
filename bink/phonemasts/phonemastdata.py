ace9bfb7700ab7e6ecda0d7f06391bbd389d80f1
Ian Beamish <ianbeami@gmail.com>
2017-10-08 12:53:35 +0100

    failing tests

--- bink/phonemasts/phonemastdata.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def open_csv
-    with open('MobilePhoneMasts.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in reader:
-            print(row)
--- bink/phonemasts/tests.py
+++ bink/phonemasts/tests.py
@@ -1,9 +1,21 @@
 import unittest
-from parse_csv import read_data
+from phonemastdata import read_data
 
-class CSVTest(unittest.TestCase):
+class CSVTest(unitest.TestCase):
 
     def setUp(self):
         self.data = "MobilePhoneMasts.csv"
 
-    
+    def test_csv_read_data_titles(self):
+        self.assertEqual(
+            read_data(self.data)[0],['Property', 'Property Address [1]', 'Property Address [3]', 'Property Address [4]', 'Unit Name', 'Tenant Name', 'Lease Start Date', 'Lease End Date', 'Lease Years', 'Current Rent']
+            )
+
+    def test_csv_read_data_property_name(self):
+        self.assertEqual(read_data(self.data)[1][0], 'Beecroft Hill')
+
+    def test_csv_read_current_rent(self):
+        self.assertEqual(read_data(self.data)[5][0], '12000')
+
+if __name__ == '__main__':
+    unittest.main()
